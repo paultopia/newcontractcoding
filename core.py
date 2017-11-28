1d2db775c4c62e7fa8334979702a6133b4312a3d
Paul Gowder <paultopia@users.noreply.github.com>
2017-11-27 21:39:33 -0600

    setup for markdown (NEED TO ADD LIBRARY)

--- core.py
+++ core.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_heroku import Heroku
+from flaskext.markdown import Markdown  # TODO: add this extension with pipenv install Flask-Markdown
 core = Flask(__name__)
+Markdown(core)
 core.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(core)
 heroku = Heroku(core)
