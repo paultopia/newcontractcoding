89fbf127d2ecf10d0fd952f5bc600128b3ff479e
Paul Gowder <paul.gowder@gmail.com>
2017-12-15 16:34:47 -0600

    try to make gunicorn be nice

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from core import core
+from core import core as app
 import os
 is_local = bool(os.environ.get("NEW_CONTRACT_CODING_LOCAL"))
 
 if __name__ == '__main__':
     if is_local:
-        core.debug = True  # comment out for prod
-    core.run()
+        app.debug = True  # comment out for prod
+    app.run()
