f320f32c5c049785ce9c8d02b79fd4221cff73b8
Paul Gowder <paul.gowder@gmail.com>
2018-04-26 17:45:14 -0500

    tests work, alas, can't replicate race

--- raceconditiontest.py
+++ raceconditiontest.py
@@ -1,5 +1,49 @@
 from testhelperclasses import *
+import concurrent.futures
 
-class TestSyncSequentialUpdate(TestStateful):
-    def test_throws(self):
-        self.assertEqual(1, 2)
+def firstcontract(response):
+    return "a (very short)" in response["content"]
+
+def secondcontract(response):
+    return "another (very short" in response["content"]
+
+headers=make_auth_header("gowder", "secret")
+
+def get_root_page(self):
+    response = self.client.get("/", headers=headers)
+    return {"type": "root page", "content": str(response.data)}
+
+def post_first_contract(self):
+    response = self.client.post("/enter-data", headers=headers,
+                               data={"1": "no",
+                                     "2": "yes",
+                                     "contract_id": "1"})
+    return {"type": "post", "content": str(response.data)}
+
+class TestSyncUpdate(TestStateful):
+    """confirm that when run synchronously, user who fetches
+    one contract and submits it gets new contract on
+    next fetch, not old contract again.
+    """
+    def test_sync(self):
+        firstpage = get_root_page(self)
+        self.assertTrue(firstcontract(firstpage))
+        self.assertFalse(secondcontract(firstpage))
+        post = post_first_contract(self)
+        secondpage = get_root_page(self)
+        self.assertTrue(secondcontract(secondpage))
+        self.assertFalse(firstcontract(secondpage))
+
+class TestThreadedUpdate(TestStateful):
+    def test_threads(self):
+        firstpage = get_root_page(self)
+        self.assertTrue(firstcontract(firstpage))
+        self.assertFalse(secondcontract(firstpage))
+        pool = concurrent.futures.ThreadPoolExecutor(max_workers=2)
+        for f in concurrent.futures.as_completed([pool.submit(post_first_contract, self), pool.submit(get_root_page, self)]):
+            res = f.result()
+            if res["type"] == "root page":
+                self.assertTrue(secondcontract(res))
+                self.assertFalse(firstcontract(res))
+
+
